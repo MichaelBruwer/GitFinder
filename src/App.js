9d9b8d5b61eb8f26eb7dc59bbf05da04a5662f7f
MichaelBruwer <michaelbruwer21@gmail.com>
2021-01-29 10:46:07 +0200

    changes
    updated packages tweaked colors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/layout/Navbar';
 import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import Home from './components/pages/Home';
-import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
@@ -13,26 +12,25 @@ import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
-  return (
-    <GithubState>
-      <AlertState>
-        <Router>
-          <div className='App'>
-            <Navbar />
-            <div className='container'>
-              <Alert />
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/user/:login' component={User} />
-                <Route component={NotFound} />
-              </Switch>
-            </div>
-          </div>
-        </Router>
-      </AlertState>
-    </GithubState>
-  );
+	return (
+		<GithubState>
+			<AlertState>
+				<Router>
+					<div className='App bg'>
+						<Navbar />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/user/:login' component={User} />
+								<Route component={NotFound} />
+							</Switch>
+						</div>
+					</div>
+				</Router>
+			</AlertState>
+		</GithubState>
+	);
 };
 
 export default App;
--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const NotFound = () => {
-  return (
-    <div>
-      <h1>Not Found</h1>
-      <p className='lead'>The page you are looking for does not exist...</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>404</h1>
+			<p className='lead'>Page Not Found</p>
+		</div>
+	);
 };
 
 export default NotFound;
